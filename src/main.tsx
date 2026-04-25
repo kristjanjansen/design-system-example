210b2b9818b37779a8918dbadb507b9b30e90bbb
Kristjan Jansen <Kristjan.Jansen@enefit.ee>
2026-04-26 00:57:36 +0300

    Update button page, remove icon examples

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,11 @@ import { createRoot } from "react-dom/client";
 import "@fontsource-variable/inter";
 import "@fontsource-variable/roboto-flex/full.css";
 import "./app.css";
-import "@kristjanjansen/design-system/css/style.css";
+import "@kristjanjansen/design-system/style.css";
+import "@kristjanjansen/design-system/brand1-light.css";
+import "@kristjanjansen/design-system/brand1-dark.css";
+import "@kristjanjansen/design-system/brand2-light.css";
+import "@kristjanjansen/design-system/brand2-dark.css";
 import { App } from "./App.tsx";
 
 createRoot(document.getElementById("app")!).render(
--- src/pages/ButtonPage.tsx
+++ src/pages/ButtonPage.tsx
@@ -13,23 +13,18 @@ export function ButtonPage() {
 
       <div className="flex gap-3 flex-wrap">
         <Button size="small">Primary</Button>
-        <Button size="small" variant="secondary">
-          Secondary
-        </Button>
-        <Button size="small" variant="tertiary">
-          Tertiary
-        </Button>
+        <Button size="small" variant="secondary">Secondary</Button>
+        <Button size="small" variant="tertiary">Tertiary</Button>
       </div>
 
       <div className="flex gap-3 flex-wrap">
         <Button disabled>Primary</Button>
-        <Button variant="secondary" disabled>
-          Secondary
-        </Button>
-        <Button variant="tertiary" disabled>
-          Tertiary
-        </Button>
+        <Button variant="secondary" disabled>Secondary</Button>
+        <Button variant="tertiary" disabled>Tertiary</Button>
+        <Button variant="danger" disabled>Danger</Button>
       </div>
+
+      <Button className="ds-button--full-width">Full width button</Button>
     </div>
   );
 }
